6fe267a937d00d6205a812825a639a4f41bc7d78
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-27 13:46:09 +0200

    etet

---
+++ Banga/BangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BangaApp.swift
+//  Banga
+//
+//  Created by Apprenant 08 on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
