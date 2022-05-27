4f44c66d900008a21919efcc0cc27bb234a94ec8
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-27 13:50:59 +0200

    emi

---
+++ Banga/EmiView.swift
@@ -0,0 +1,20 @@
+//
+//  EmiView.swift
+//  Banga
+//
+//  Created by Apprenant 08 on 27/05/2022.
+//
+
+import SwiftUI
+
+struct EmiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmiView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmiView()
+    }
+}
